789ba061b8e2dc76d52f31a38046d4365cf41d6c
mkkieru <mkieru55@.com>
2021-09-05 15:16:20 +0300

    add button on create

--- app/src/main/java/com/example/prettyalphas/MainActivity.java
+++ app/src/main/java/com/example/prettyalphas/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.prettyalphas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button mViewAllProperties;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mViewAllProperties = (Button) findViewById(R.id.viewall);
+        mViewAllProperties.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            //Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this,ViewAll.class);
+            startActivity(intent);
+            }
+        });
     }
 }
